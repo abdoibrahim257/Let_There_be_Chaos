fe643375951652d594dc8f1dbc25b18e81e70fb8
Zeyad Mansour <zeyadmansour2@gmail.com>
2022-08-07 13:44:03 +0200

    background edits in level 2

--- Let There Be Chaos/Assets/Scripts/Chase.cs
+++ Let There Be Chaos/Assets/Scripts/Chase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Chase : StateMachineBehaviour
 {
@@ -10,20 +11,21 @@ public class Chase : StateMachineBehaviour
     private float timer = 0;
     //private float attacktimer = 0;
     [SerializeField] public float ExitDetectRadius;
-    //private GunController gunc;
+
+    // [Header("Custom Event")]            // 3lshan ast3ml el events
+    // public UnityEvent customEvent;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        player = GameObject.FindWithTag("Player").transform;
-    //    gunc = animator.GetComponent<GunController>();
-
-    //    gunc.inventory[0]._isActive = true;
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.transform.up = player.position - animator.transform.position;
         animator.transform.position = Vector2.MoveTowards(animator.transform.position, player.position, ChaseSpeed * Time.deltaTime);
+
+        //customEvent?.Invoke();
         
         StopChase(animator);
         //AttackControl();
